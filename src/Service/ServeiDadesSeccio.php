0e33cc9468f1d3af6c8c02b0b02fbfb40d7d5bd6
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-16 12:34:25 +0100

    Añadir imagenes de las secciones.

--- src/Service/ServeiDadesSeccio.php
+++ src/Service/ServeiDadesSeccio.php
@@ -9,28 +9,28 @@ class ServeiDadesSeccio
             "codi" => "1",
             "nom" => "Ordinadors",
             "descripcio" => "Secció dedicada a ordinadors i accessoris",
-            "any" => "2025",
+            "imatge" => "/imgs/portatil.png",
             "articles" => array("Portàtils", "Sobretaules", "Teclats", "Ratolins")
         ),
         array(
             "codi" => "2",
             "nom" => "Telèfons",
             "descripcio" => "Secció dedicada a telèfons mòbils i accessoris",
-            "any" => "2025",
+            "imatge" => "/imgs/movil.png",
             "articles" => array("Smartphones", "Fundes", "Carregadors", "Auriculars")
         ),
         array(
             "codi" => "3",
             "nom" => "Consoles",
             "descripcio" => "Secció de consoles i videojocs",
-            "any" => "2025",
+            "imatge" => "/imgs/consola.png",
             "articles" => array("PlayStation", "Xbox", "Nintendo Switch", "Videojocs")
         ),
         array(
             "codi" => "4",
             "nom" => "Monitors",
             "descripcio" => "Secció de monitors i pantalles",
-            "any" => "2025",
+            "imatge" => "/imgs/ordinador.png",
             "articles" => array("Monitors Full HD", "Monitors 4K", "Pantalles tàctils", "Suports per a monitors")
         )
     );
